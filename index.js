fc303cd4bb36aad3c6d4cb1ad0e312b6e91b72c4
ell20-sys <elliotawe@outlook.com>
2024-03-05 09:39:52 +0000

    Adding suitable comments to code

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
+// Automatically loading .env variables with dotenv
 require('dotenv').config();
-// console.log(process.env) 
+
+// Import dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./modules/product.module');  
 const app = express();
 
+// Import modules
+const Product = require('./modules/product.module');
+
+// Middleware
 app.use(express.json());
 
-// env vars
+// Environment variables
 const port = process.env.PORT;
 const dbUrl = process.env.DATABASE_URL;
 
-
+// Define routes
+// Root route
 app.get('/', (req, res) => {
-  res.send('Hello from Node API server updated')
-})
-app.get('/api/products', async (req, res)=>{
+  res.send('Hello from Node API server updated');
+});
+
+// Retrieve all products
+app.get('/api/products', async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({message: error.message});
+    res.status(500).json({ message: error.message });
   }
-})
-app.get('/api/product/:id', async (req, res)=>{
+});
+
+// Retrieving a single product by ID
+app.get('/api/product/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const products = await Product.findById(id);
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-})
-app.post('/api/products', async (req, res) => {
-  try{
-    const product = await Product.create(req.body);
-    res.status(200).json(product)
+    const product = await Product.findById(id);
+    res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-})
+});
 
-mongoose.connect(dbUrl)
-.then(() => {
-  console.log('Connected to MongoDB')
-  app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+// Creating a new product
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Connecting to MongoDB and starting the server
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
   })
-})
-.catch(err => console.error('Error connecting to MongoDB', err));
+  .catch((err) => console.error('Error connecting to MongoDB', err));
