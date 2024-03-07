4ab28d80ec300998c3f9a0c0b73ffd2b37c4d3ea
ell20-sys <elliotawe@outlook.com>
2024-03-07 17:31:56 +0000

    add: Delete endpoint added

--- index.js
+++ index.js
@@ -61,12 +61,27 @@ app.put('/api/product/:id', async (req, res) => {
     if (!product){
       return res.status(404).json({message: "Product not found"})
     }
-    const updatedProdcut = await Product.findById(id);
-    res.status(200).json(updatedProdcut);
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
+// Deleting a product
+
+app.delete('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product){
+      return res.status(404).json({message: "Product not found"})
+    }
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
 
 
 // Connecting to MongoDB and starting the server
